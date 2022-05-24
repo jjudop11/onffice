902e9c4d6d55a7edece87d42e01f7c5327211ae0
youngjin12 <kyj00504@gmail.com>
2022-05-24 09:23:11 +0900

    2022 05 24 김영진

---
+++ src/main/java/com/uni/spring/chat/model/service/ChatServiceImpl.java
@@ -0,0 +1,29 @@
+package com.uni.spring.chat.model.service;
+
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.uni.spring.chat.model.dao.ChatDao;
+import com.uni.spring.chat.model.dto.Chat;
+
+
+
+@Service
+public class ChatServiceImpl implements ChatService{
+
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Autowired
+	private ChatDao chatDao;
+	
+	@Override
+	public ArrayList<Chat> selectChatRoomList() {
+		// TODO Auto-generated method stub
+		return chatDao.selectChatRoomList(sqlSession);
+	}
+
+}
