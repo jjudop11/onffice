9b213bf16e49d1894e24957763f6620aa00fdfde
gimmary <93731106+gimmary@users.noreply.github.com>
2022-06-08 09:45:05 +0900

    Update and rename MeetingroomService.java to MeetingRoomService.java

--- src/main/java/com/uni/spring/meetingRoom/model/service/MeetingRoomService.java
+++ src/main/java/com/uni/spring/meetingRoom/model/service/MeetingRoomService.java
@@ -1,24 +1,24 @@
-package com.uni.spring.meetingroom.model.Service;
+package com.uni.spring.meetingRoom.model.service;
 
 import java.util.ArrayList;
 
 import com.uni.spring.common.PageInfo;
-import com.uni.spring.meetingroom.model.dto.Meetingroom;
-import com.uni.spring.meetingroom.model.dto.Reserveroom;
+import com.uni.spring.meetingRoom.model.dto.MeetingRoom;
+import com.uni.spring.meetingRoom.model.dto.ReserveRoom;
 
-public interface MeetingroomService {
+public interface MeetingRoomService {
 
-	int selectRoomsetUser(String roomsetUserId);
+	int selectRoomSetUser(String roomsetUserId);
 
 	// ArrayList<Meetingroom> selectList(PageInfo pi, int userCNo);
 
-	int insertMeetingroom(Meetingroom m);
+	int insertMeetingRoom(MeetingRoom m);
 
 	int selectRoomListCount(int userCNo);
 
-	ArrayList<Meetingroom> selectRoomList(PageInfo pi, int userCNo);
+	ArrayList<MeetingRoom> selectRoomList(PageInfo pi, int userCNo);
 
-	ArrayList<Meetingroom> selectList(int userCNo);
+	ArrayList<MeetingRoom> selectList(int userCNo);
 
 	// 회의실 번호 중복체크
 	int roomNoCheck(String roomNo);
@@ -34,14 +34,14 @@ public interface MeetingroomService {
 	double selectEndKey(String endTime);
 
 	// 회의실 예약
-	int reserveRoom(Reserveroom room);
+	int reserveRoom(ReserveRoom room);
 
 	// 회의실 예약시 roomNo 얻기
 	String selectRoomNo(String selectRoom);
 
 	// 회의실 예약시 이미 예약된 목록 얻기
-	ArrayList<Reserveroom> checkReservedRooms(String roomNo, String date);
+	ArrayList<ReserveRoom> checkReservedRooms(String roomNo, String date);
 
-	ArrayList<Reserveroom> selectReservedRooms(int cNo, String date);
+	ArrayList<ReserveRoom> selectReservedRooms(int cNo, String date);
 
 }
