8cc786b266f0a91f095dffd156c4ee9bb2fbcea2
youngjin12 <kyj00504@gmail.com>
2022-06-17 09:19:24 +0900

    2022 06 17 김영진

--- src/main/java/com/uni/spring/surveyBoard/model/dao/SurveyBoardDao.java
+++ src/main/java/com/uni/spring/surveyBoard/model/dao/SurveyBoardDao.java
@@ -1,8 +1,13 @@
 package com.uni.spring.surveyBoard.model.dao;
 
+import java.util.ArrayList;
+
+import org.apache.ibatis.session.RowBounds;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.uni.spring.common.PageInfo;
+import com.uni.spring.member.model.dto.Member;
 import com.uni.spring.surveyBoard.model.dto.SurveyBoard;
 
 @Repository
@@ -11,6 +16,48 @@ public class SurveyBoardDao {
 	public int insertSurveyBoard(SqlSessionTemplate sqlSession, SurveyBoard sb) {
 		// TODO Auto-generated method stub
 		return sqlSession.insert("surveyBoardMapper.insertSurveyBoard" , sb);
+	}
+
+	public ArrayList<Member> selectDeptList(SqlSessionTemplate sqlSession, int cNo) {
+		// TODO Auto-generated method stub
+		return (ArrayList)sqlSession.selectList("surveyBoardMapper.selectDeptList", cNo);
+	}
+
+	public void insertSBTargetDept(SqlSessionTemplate sqlSession, ArrayList<SurveyBoard> list) {
+		// TODO Auto-generated method stub
+		sqlSession.insert("surveyBoardMapper.insertSBTargetDept" , list);
+	}
+
+	public void insertSBInquiry(SqlSessionTemplate sqlSession, SurveyBoard sb) {
+		// TODO Auto-generated method stub
+		sqlSession.insert("surveyBoardMapper.insertSBInquiry" , sb);
+	}
+
+	/*
+	public void insertSBQuestion(SqlSessionTemplate sqlSession, ArrayList<SurveyBoard> list) {
+		// TODO Auto-generated method stub
+		sqlSession.insert("surveyBoardMapper.insertSBQuestion" , list);
+	}
+	 */
+	public void insertSBQuestion(SqlSessionTemplate sqlSession, SurveyBoard sb) {
+		// TODO Auto-generated method stub
+		sqlSession.insert("surveyBoardMapper.insertSBQuestion" , sb);
+	}
+
+	public int selectHomeListCount(SqlSessionTemplate sqlSession, SurveyBoard sb) {
+		// TODO Auto-generated method stub
+		return sqlSession.selectOne("surveyBoardMapper.selectSBHomeListCount", sb);
+	}
+
+	public ArrayList<SurveyBoard> selectHomeList(SqlSessionTemplate sqlSession, SurveyBoard sb, PageInfo pi) {
+		// TODO Auto-generated method stub
+		if(pi == null) {
+			return (ArrayList)sqlSession.selectList("surveyBoardMapper.selectSBHomeList", sb);
+		} else {
+			int offset = (pi.getCurrentPage() - 1) * pi.getBoardLimit();
+			RowBounds rowBounds = new RowBounds(offset, pi.getBoardLimit());
+			return (ArrayList)sqlSession.selectList("surveyBoardMapper.selectSBHomeList", sb, rowBounds);
+		}
 	}
 
 }
--- src/main/webapp/WEB-INF/views/surveyBoard/surveyBoardList.jsp
+++ src/main/webapp/WEB-INF/views/surveyBoard/surveyBoardList.jsp
@@ -10,28 +10,27 @@
 
 	
 	.cast_surveyBoardList{
-		border-top:1px solid red;
-		width:75%;
+
 		height:700px;
-		position:absolute;
-		padding:30px 20px;
+		padding:30px 20px; 
+		display:flex;
+	}
+	
+	.main-cast_lists{
+		width:80%;
+		left:0px;
 	}
 	
 	.side-cast_listOption{
-		border-left:1px solid blue;
+		border-left:1px solid rgb(159, 160, 167);
 		width:20%;
 		height:700px;
-		position:absolute;
 		right:0px;
 		padding:20px 10px;
-	
-	}
-	
-	.option:hover{
-		border:1 solid rgb(70, 98, 223);
-		border-radius:70%;
-	}
 
+	}
+	
+	
 </style>
 
 
@@ -40,16 +39,17 @@
 <jsp:include page="../common/menubar.jsp"/>
 
 	<div id="app">
-		<div id="main" style="border:1 solid black; position:relative;">
-			<div class="cast_surveyBoardList">
-				<div class="sb_header" style="border-bottom:1px solid black">
-					<h3>설문 게시판</h3>
+		<div id="main">
+			<div class="cast_surveyBoardList" >
+				<div class="main-cast_lists">
+					<div class="sb_header" style="border-bottom:1px solid rgb(159, 160, 167); height:70px; padding:30px 0 0 10px;">
+						<h3>설문 게시판</h3>
+					</div>
+					<div class="sb_body">
+						<h5 class="kind_list"></h5>
+					</div>
 				</div>
-				<div class="sb_body">
-					
-				</div>
-			</div>
-			<div class="side-cast_listOption">
+				<div class="side-cast_listOption" >
 				<button id="go_createBoard" class="btn btn-outline-secondary" value="설문 작성" style="width:200px; height:50px; font-size:15px;">설문 작성</button>
 				<div class="optionList" style="margin:40px 0 0 10px; font-size:18px; font-weight:bold">
 						<div class="option"><a href="#" class="endBoardList"><i class="bi bi-stop-fill"></i> 종료된 설문</a></div>
@@ -58,6 +58,7 @@
 						<div class="option"><a href="#" class="endBoardList"><i class="bi bi-stop-fill"></i> 홈으로</a></div>
 				</div>
 			</div>
+			</div>
 		</div>
 	</div>
 	
@@ -65,25 +66,22 @@
 	
 	<script>
 		
+	
+	$(function(){
+		
+		selectSurveyBoard(1)
+		
+
+		
+	})
+	
 	// 설문 작성 페이지 이동
 	$('#go_createBoard').click(function(){
 	
 		location.href="createSurveyBoardForm";
 		
 	})
-	
-	
-	
-	
-	
-		$(function(){
-		
-			//selectSurveyBoard(1)
-			
-	
-			
-		})
-		
+
 		function selectSurveyBoard(num){
 				
 				$.ajax({
